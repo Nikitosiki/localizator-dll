c8847eaee4e9702c8e9ce694ca616594d1884d59
Nikitosiki <nikita.lol.d9@gmail.com>
2023-05-24 12:38:15 +0300

    Test project 1 - completed

--- LocalizatorApp/LocalizatorApp.cpp
+++ LocalizatorApp/LocalizatorApp.cpp
@@ -1,22 +1,30 @@
 ﻿#include <iostream>
+#include "DllFunctions.h"
 
 using namespace std;
 
 int main()
 {
+	while (true)
+	{
+		cout << "Path to XML file:";
+		std::string pathFile;
+		std::getline(std::cin, pathFile);
+		if (!ReadXML(pathFile))
+		{
+			cout << "Error load file!";
+			continue;
+		}
+		else
+			break;
+	}
+
 	std::string keyValue;
 	while (true)
 	{
-		cout << "\n\nWrite the key:\n";
+		cout << "\n\nWrite the key:";
 		cin >> keyValue;
+		cout << "Value: " + GetWorld(keyValue);
 
-		/*if ()
-		{
-			cout << "\nValue:";
-		}
-		else
-		{
-			cout << "\nNo value or key found!"
-		}*/
 	}
 }
